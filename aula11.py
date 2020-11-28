7e4ac30ab1f117a84fa83b90a85b77d761abf9a6
Klayver Ximenes <klayverx@gmail.com>
2020-11-27 22:12:09 -0300

    commit

---
+++ aula11.py
@@ -0,0 +1,31 @@
+# Tratamento de erros
+
+lista = [1, 10]
+arquivo = open('teste.txt', 'r')
+
+try:
+    texto = arquivo.read()
+    divisao = 10 / 1
+    numero = lista[1]
+    print("Fechando o arquivo")
+    arquivo.close()
+
+# Encadeamento de except
+except ZeroDivisionError:
+    print("Não é possível realizar uma divisão por zero")
+except ArithmeticError:
+    print("Houve um erro ao executar uma operação eritmética")
+except ZeroDivisionError:
+    print("Não é possível realizar uma divisão por 0!")
+except IndexError:
+    print("Erro ao acessar um índice inválido da lista")
+except Exception as ex:
+    print("Erro desconhecido. Erro: {}" .format(ex))
+else:
+    print("Executa quando não ocorre exceção")
+
+# Sempre executa, independente dos erros
+finally:
+    print('Sempre executa')
+    print('Fechando o arquivo')
+    arquivo.close()
